319f4483494b6046a213d0819583483a101b708b
Puppet SE User <noreply@puppet.com>
2020-09-23 09:20:49 -0500

    final update of umask_check.sh script

--- scripts/umask_check.sh
+++ scripts/umask_check.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-grep umask /etc/bashrc
-grep umask /etc/logindefs
-grep umask /etc/profile
-grep umask /etc/csh.cshrc
+#Look for umask settings in list of files
+
+#Specify array of file names
+files=( "/etc/bashrc" "/etc/login.defs" "/etc/profile" "/etc/csh.cshrc")
+
+#Iterate over array to search for umask and output filename and results
+for i in "${files[@]}"
+do
+    echo $i
+    echo -----
+    grep umask $i
+    echo -----
+done
